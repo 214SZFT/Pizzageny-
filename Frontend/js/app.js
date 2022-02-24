259ab58abca56a55958b7caa92f1f27eabbdd13e
Mikeex99 <szevaldmiki99@gmail.com>
2022-02-24 11:33:53 +0100

    az adtatok változtatása statisztika kiíratása

--- Frontend/js/app.js
+++ Frontend/js/app.js
@@ -2,4 +2,5 @@ var app = new angular.module('pizzaApp', []);
 
 app.run(function ($rootScope) {
     $rootScope.title = 'Pizza statisztika';
+    $rootScope.osszosszeg=0;
 });
--- Frontend/js/statisztikaCtrl.js
+++ Frontend/js/statisztikaCtrl.js
@@ -4,3 +4,15 @@ app.controller('satisztikaCtrl', function ($scope, $rootScope, dbFactory) {
         console.log(res.data);
     });
 });
+app.controller('statCtrl', function ($scope, $rootScope, dbFactory) {
+    dbFactory.selectAll('eladasok').then(function (res) {
+        $scope.records = res.data;
+        var szamolo=0;
+        for(let i=0;i<$scope.records.length;i++){
+            szamolo+=parseInt($scope.records[i].ossz);
+        }
+        $rootScope.osszosszeg=szamolo;
+        
+        console.log(res.data);
+    });
+});

11beda89655347caac514e32ada9db8f56d5b8d9
thespekky <bandras123456@gmail.com>
2022-02-28 12:27:43 +0100

    néves kiiratás

--- Frontend/js/kosarCtrl.js
+++ Frontend/js/kosarCtrl.js
@@ -1,11 +1,22 @@
 app.controller('kosarCtrl',function($scope,$rootScope,dbFactory)
 {
+    $scope.pizzak=[];
     $scope.values=[
-        {id:1,db:3},
-        {id:2,db:2},
-        {id:3,db:1},
-        {id:1,db:3},
+        {pizzaID:5,db:3},
+        {pizzaID:6,db:2},
+        {pizzaID:7,db:1},
+        {pizzaID:5,db:3},
     ];
-    console.log($scope.values);
+    dbFactory.selectAll('pizzak').then(function(res) {
+        $scope.pizzak = res.data;
+
+    $scope.kiras=[];
+    for(var i=0;i<$scope.values.length;i++)
+    {
+        let index=$scope.pizzak.findIndex(pizza=>pizza.ID==$scope.values[i].pizzaID);
+        $scope.kiras.push({nev:$scope.pizzak[index].nev,db:$scope.values[i].db});
+    }
+    console.log($scope.pizzak);
     localStorage.setItem('rendelesek',JSON.stringify($scope.values));
+});
 })
